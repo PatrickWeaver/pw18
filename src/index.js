d8c6d8eeb78523bb824c6b63b13099b0249a469c
Glitch (pw18) <none>
2018-09-19 13:11:52 +0000

    Remove markdown filter

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import * as settings from '../settings'
 import Vue from 'vue/dist/vue.js'
 import VueRouter from 'vue-router'
-import marked from 'marked'
 
 import app from './components/App.vue'
 
 import router from './router'
 
 Vue.use(VueRouter)
-
-Vue.filter('fromMarkdown', function (value) {
-  if (!value) return ''
-  return marked(value.toString())
-})
 
 const vm = new Vue({
   el: '#vm',
